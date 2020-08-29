775bda58494afdfced7b8c6398db8633e6e4f8eb
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-08-29 04:57:50 +0000

    Shovel some of them there there over there

--- generate-character.js
+++ generate-character.js
@@ -1,16 +1,7 @@
-import sample from './lib/sample.js'
-import name from './lib/name.js'
-const ideals = ['order', 'justice', 'heroism', 'compassion', 'generosity',
-'pleasure', 'pragmatism', 'honor', 'power', 'salvation', 'the ends']
+import name from "./lib/name.js";
+import ideal from "./lib/ideal.js";
+import flawed from "./lib/flawed.js";
 
-
-const flaws = ['fearful', 'megalomaniac', 'idiot', 'impish', 'oblivious',
-'thief', 'hedonist', 'liar', 'reckless', 'wrathful', 'vain']
-
-
-const ideal = () => sample(ideals)
-const flawed = () => sample(flaws)
 console.log(
-  `${name()}, the ${flawed()}; seeker of ${ideal()}`
-)
-
+  `${name()}, the ${flawed()}; seeker of ${ideal()}`,
+);
