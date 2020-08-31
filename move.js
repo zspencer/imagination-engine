f4fbffbebbbec79bab20409efa67e8d8c7ebf166
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-08-31 00:25:07 +0000

    Movement generates empty markdown files for the tile

--- move.js
+++ move.js
@@ -3,6 +3,20 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 
 const directions = parse(Deno.args)._
 
+const encoder = new TextEncoder("utf-8");
+const locationPath = (loc) => {
+  return `loc/loc-${loc.latitude}-${loc.longitude}.md`
+}
+const loadOrGenerate = async (loc) => {
+  try {
+    return await Deno.readFile(locationPath(loc));
+  } catch(e) {
+    if(e.name !== "NotFound") { throw e }
+    await Deno.writeFile(locationPath(loc), encoder.encode(''));
+    return await Deno.readFile(locationPath(loc));
+  }
+}
+
 const translate = (loc, direction) => {
   if(direction == "north") {
     return { ...loc, latitude: loc.latitude + 1 }
@@ -19,14 +33,15 @@ const translate = (loc, direction) => {
     return { ...loc, longitude: loc.longitude - 1 }
   }
 }
+
 const loc = await readJson('loc.json');
 const newLoc = directions.reduce((loc, direction) => translate(loc, direction), loc)
 
 const decoder = new TextDecoder("utf-8");
-const des = await Deno.readFile(`loc/loc-${newLoc.latitude}-${newLoc.longitude}.md`);
+const destination = loadOrGenerate(newLoc)
 
 console.log(newLoc)
-console.log(decoder.decode(des));
+console.log(decoder.decode(destination));
 
 await writeJson('loc.json', newLoc);
 
