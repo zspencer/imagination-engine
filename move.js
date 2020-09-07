7c9a6230c8b9ebdb16f7241db1e4855c05968993
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-09-07 21:02:39 +0000

    Forgot to format before a commit

--- move.js
+++ move.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S deno run --no-check --allow-write --allow-read --unstable
 
 import { readJson, writeJson, parseFlags } from "./deps.bundle.js";
-import { loadOrGenerate } from './lib/tile.js'
-import { translateAll } from './lib/coordinate.js'
+import { loadOrGenerate } from "./lib/tile.js";
+import { translateAll } from "./lib/coordinate.js";
 
 const directions = parseFlags(Deno.args)._;
 const loc = await readJson("loc.json");
-const newLoc = translateAll(loc, directions)
+const newLoc = translateAll(loc, directions);
 
 const destination = await loadOrGenerate(newLoc);
 
