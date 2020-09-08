11a2ffcae463ebe172394c286be65561f1bb725a
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-09-08 02:58:13 +0000

    🛠 Scene generation is now done in a library
    This sets the stage for us to generate a scene with more complex things
    going on, or by considering the context of current tile.

--- generate-scene.js
+++ generate-scene.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env -S deno run --no-check --allow-write --allow-read
+#!/usr/bin/env -S deno run --allow-write --allow-read
 
 import { parseFlags } from "./deps.bundle.js";
-import sample from "./lib/sample.js";
-import { creatures } from "./lib/world.js";
+import { generate } from "./lib/scene.js";
 
 const biomes = parseFlags(Deno.args)._;
 
-console.log(sample(await creatures(biomes)).textContent);
+const scene = generate(biomes);
+scene.creatures.forEach((c) => console.log(c.textContent));
