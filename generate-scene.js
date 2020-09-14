ed01d6228d11f9df382c0e4321d25e2fd0c6676a
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-09-14 03:24:16 +0000

    World definitions may pull in supplements

--- generate-scene.js
+++ generate-scene.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-write --allow-read
+#!/usr/bin/env -S deno run --allow-write --allow-read --allow-net
 
 import { parseFlags } from "./lib/cli.ts";
 import { generate } from "./lib/scene.js";
