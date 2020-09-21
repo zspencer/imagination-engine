4cbac4e8954ff4b6452188fe496de0559c958d7e
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-09-21 01:51:54 +0000

    🐞Prevent prompting before rendering

--- client.js
+++ client.js
@@ -3,9 +3,9 @@ import { prompt, execute, render } from "./lib/cli.ts";
 
 let lastCommand = "";
 while (lastCommand != "quit") {
-  const input = await prompt();
+  const input = await prompt("what next?");
   let [cmd, ...args] = input.split(" ");
-  execute(cmd, args)
-    .then(render(cmd));
-  lastCommand = cmd;
+  await execute(cmd, args)
+    .then(render(cmd))
+    .then(() => lastCommand = cmd);
 }
