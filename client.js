f75da981f588c0da27f2abdc5f9084bbfb0938bc
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2020-10-01 01:33:56 +0000

    Expose more of the Creature attributes

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ import { prompt, execute, render } from "./lib/cli.ts";
 
 let lastCommand = "";
 while (lastCommand != "quit") {
-  const input = await prompt("what next?");
+  const input = await prompt("what next? ");
   let [cmd, ...args] = input.split(" ");
   await execute(cmd, args)
     .then(render(cmd))
